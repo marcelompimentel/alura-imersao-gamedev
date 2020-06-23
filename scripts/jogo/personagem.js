317ca2c177708f1cb7b0cdfca0fcc4a8031378f0
Marcelo Pimentel <marcelompimentel@gmail.com>
2020-06-23 09:12:09 -0300

    Refatoração da matriz de movimentação do personagem

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,30 +1,27 @@
 class Personagem {
 	constructor(imagem) {
 		this.imagem = imagem;
-		this.matriz = [
-			[0, 0],
-			[220, 0],
-			[440, 0],
-			[660, 0],
-			[0, 270],
-			[220, 270],
-			[440, 270],
-			[660, 270],
-			[0, 540],
-			[220, 540],
-			[440, 540],
-			[660, 540],
-			[0, 810],
-			[220, 810],
-			[440, 810],
-			[660, 810]
-		];
+
+		this.personagem_largura = 220;
+		this.personagem_altura = 270;
+
+		this.matriz = new Array();
+		for(var j = 0; j < 4; j++) {
+			for(var k = 0; k < 4; k++) {
+				this.matriz.push([this.personagem_largura * k, this.personagem_altura * j]);
+			}
+		}
+
 		this.frame_atual = 0;
 	}
 
 
 	exibe() {
-		image(this.imagem, 0, height - 135, 110, 135, this.matriz[this.frame_atual][0], this.matriz[this.frame_atual][1], 220, 270);
+		image(this.imagem,
+			0, height - this.personagem_altura / 2,
+			this.personagem_largura / 2, this.personagem_altura / 2,
+			this.matriz[this.frame_atual][0], this.matriz[this.frame_atual][1],
+			this.personagem_largura, this.personagem_altura);
 
 		this.anima();
 	}
